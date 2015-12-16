2cbb09d712607208c092927e50c0d0aaf7493ef0
Mark Schäfer <mark.schaefer@kv-telematik.de>
2015-12-16 11:38:23 +0100

    Defaultuser removed, default is now without user

--- src/main/java/net/kevinboone/apacheintegration/amqutil/Cmd.java
+++ src/main/java/net/kevinboone/apacheintegration/amqutil/Cmd.java
@@ -6,6 +6,7 @@ Distributed under the terms of the GPL v2.0
 ==========================================================================*/
 
 package net.kevinboone.apacheintegration.amqutil;
+
 import org.slf4j.*;
 import java.io.*;
 import org.apache.commons.cli.*;
@@ -17,137 +18,141 @@ import org.apache.commons.io.FileUtils;
  */
 public abstract class Cmd
 {
-  protected Options options = null;
-  protected CommandLine cl = null; 
-  protected Logger logger = null;
+    protected Options options = null;
+    protected CommandLine cl = null;
+    protected Logger logger = null;
 
-  public static String DEFAULT_HOST = "localhost";
-  public static int DEFAULT_PORT = 61616;
-  public static String DEFAULT_USER= "admin";
-  public static String DEFAULT_PASS= "admin";
-  public static String DEFAULT_DESTINATION = "__test_destination";
+    public static String DEFAULT_HOST = "localhost";
+    public static int DEFAULT_PORT = 61616;
+    public static String DEFAULT_USER = null;
+    public static String DEFAULT_PASS = null;
+    public static String DEFAULT_DESTINATION = "__test_destination";
 
-  public Cmd ()
+    public Cmd()
     {
-    options = new Options();
+        options = new Options();
     }
 
-  /**
-  Set the command-line options that all commands will support. Specidic
-  commands will usually have to override this and add others. */
-  public void setupOptions()
-   {
-   options.addOption (null, "time", false, 
-      "show time to complete operation in msec");
-   options.addOption (null, "help", false, 
-      "show brief help");
-   options.addOption (null, "loglevel", true, 
-      "set log level -- error, info, etc");
-   }
-
-  public abstract int run() throws Exception; 
-
-  public void setLogger (Logger logger)
+    /**
+     * Set the command-line options that all commands will support. Specidic commands will usually
+     * have to override this and add others.
+     */
+    public void setupOptions()
     {
-    this.logger = logger;
+        options.addOption(null, "time", false,
+            "show time to complete operation in msec");
+        options.addOption(null, "help", false,
+            "show brief help");
+        options.addOption(null, "loglevel", true,
+            "set log level -- error, info, etc");
     }
 
-  /**
-  doRun wraps the (abstract) run() method in timing and logging options
-  */
-  public int doRun() throws Exception
+    public abstract int run() throws Exception;
+
+    public void setLogger(Logger logger)
     {
-    if (cl.hasOption ("help"))
-      {
-      briefHelp (System.out);
-      return 0;
-      }
-
-    boolean time = false;
-    long start = 0;
-    String _logLevel = cl.getOptionValue ("loglevel");
-    if (_logLevel != null)
-      System.setProperty ("log.level", _logLevel);
-
-    logger = LoggerFactory.getLogger 
-      ("net.kevinboone.apacheintegration");
-  
-    if (cl.hasOption ("time"))
-      time = true;
-    if (time)
-      start = System.currentTimeMillis();
-    int ret = run();
-    if (time)
-      {
-      System.out.println ("Time elapsed: " + 
-        (System.currentTimeMillis() - start) + " msec");
-      }
-    return ret;
+        this.logger = logger;
     }
 
-  public void parseArgs (String[] args)
-      throws ArgParseException
+    /**
+     * doRun wraps the (abstract) run() method in timing and logging options
+     */
+    public int doRun() throws Exception
     {
-    CommandLineParser clp = new GnuParser();
-    try
-      {
-      cl = clp.parse (options, args);
-      }
-    catch (Exception e)
-      {
-      throw new ArgParseException (e);
-      }
-    }
-
-  public abstract String getName();
-  public abstract String getShortDescription();
-  public abstract String getShortUsage();
-
-  public void showOptions ()
-    {
-    HelpFormatter formatter = new HelpFormatter();
-    formatter.printHelp ("amqutil " + getName(), options);
-    }
-
-  public void briefHelp (PrintStream p)
-    {
-    p.println (getShortUsage());
-    p.println (getShortDescription());
-    showOptions();
-    }
-
-  /**
-  Gets the ActiveMQ Connection factory from either host/port or URL.
-  URL takes precedence.
-  */
-  ActiveMQConnectionFactory getFactory (String host, int port, 
-      String url)
-    {
-    ActiveMQConnectionFactory factory = null; 
-    if (url != null && url.length() != 0)
-      {
-      factory = new ActiveMQConnectionFactory (url);
-      if (!host.equals ("localhost") || port != 61616)
+        if (cl.hasOption("help"))
         {
-        logger.warn ("Ignoring host/port arguments as a URL was specified");
+            briefHelp(System.out);
+            return 0;
         }
-      }
-    else
-      {
-      factory = new ActiveMQConnectionFactory
-          ("tcp://" + host + ":" + port);
-      }
-    return factory;
+
+        boolean time = false;
+        long start = 0;
+        String _logLevel = cl.getOptionValue("loglevel");
+        if (_logLevel != null)
+        {
+            System.setProperty("log.level", _logLevel);
+        }
+
+        logger = LoggerFactory.getLogger("net.kevinboone.apacheintegration");
+
+        if (cl.hasOption("time"))
+        {
+            time = true;
+        }
+        if (time)
+        {
+            start = System.currentTimeMillis();
+        }
+        int ret = run();
+        if (time)
+        {
+            System.out.println("Time elapsed: " +
+                (System.currentTimeMillis() - start) + " msec");
+        }
+        return ret;
     }
 
-  /** 
-  Read a file into a string
-  */
-  static String readFile (String path) throws IOException
+    public void parseArgs(String[] args)
+        throws ArgParseException
     {
-    return FileUtils.readFileToString (new File (path));
+        CommandLineParser clp = new GnuParser();
+        try
+        {
+            cl = clp.parse(options, args);
+        }
+        catch (Exception e)
+        {
+            throw new ArgParseException(e);
+        }
     }
 
+    public abstract String getName();
+
+    public abstract String getShortDescription();
+
+    public abstract String getShortUsage();
+
+    public void showOptions()
+    {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("amqutil " + getName(), options);
+    }
+
+    public void briefHelp(PrintStream p)
+    {
+        p.println(getShortUsage());
+        p.println(getShortDescription());
+        showOptions();
+    }
+
+    /**
+     * Gets the ActiveMQ Connection factory from either host/port or URL. URL takes precedence.
+     */
+    ActiveMQConnectionFactory getFactory(String host, int port,
+        String url)
+    {
+        ActiveMQConnectionFactory factory = null;
+        if (url != null && url.length() != 0)
+        {
+            factory = new ActiveMQConnectionFactory(url);
+            if (!host.equals("localhost") || port != 61616)
+            {
+                logger.warn("Ignoring host/port arguments as a URL was specified");
+            }
+        }
+        else
+        {
+            factory = new ActiveMQConnectionFactory("tcp://" + host + ":" + port);
+        }
+        return factory;
+    }
+
+    /**
+     * Read a file into a string
+     */
+    static String readFile(String path) throws IOException
+    {
+        return FileUtils.readFileToString(new File(path));
+    }
 
 }
-
